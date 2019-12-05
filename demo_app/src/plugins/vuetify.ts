8560acd66c234c6fe4c0ae5744cf2dcf6fbb7fac
CNI Apps <cniapps@protonmail.com>
2019-12-05 17:55:34 +0100

    change color to make it more zalando

--- demo_app/src/plugins/vuetify.ts
+++ demo_app/src/plugins/vuetify.ts
@@ -8,7 +8,7 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#ff9917',
+        primary: '#ff6900',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
