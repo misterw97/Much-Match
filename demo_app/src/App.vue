e63c6edd4bada3b0a272c19313c24e6de01c124c
rogerxaic <roger.miret@gmail.com>
2019-12-06 18:14:29 +0100

    add socket io between front and back.

--- demo_app/src/App.vue
+++ demo_app/src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <v-app>
     <v-content>
-      <router-view />
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-export default Vue.extend({
-  name: "App",
-  watch: {
-    $route: {
-      handler: (to, from) => {
-        document.title =
-          to.meta.title ||
-          "MuchMatch: Swiping Behaviour Study [EPFL Experience Design]";
-      },
-      immediate: true
-    }
-  }
-});
+<script lang="js">
+    import Vue from "vue";
+    import VueSocketIO from 'vue-socket.io'
+
+    Vue.use(new VueSocketIO({
+        debug: true,
+        connection: process.env.VUE_APP_WS_BASE,
+    }));
+
+    export default Vue.extend({
+        name: "App",
+        sockets: {
+            connect: function () {
+                console.log('socket connected')
+            },
+            customEmit: function (val) {
+                console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+            }
+        },
+        watch: {
+            $route: {
+                handler: (to, from) => {
+                    document.title =
+                        to.meta.title ||
+                        "MuchMatch: Swiping Behaviour Study [EPFL Experience Design]";
+                },
+                immediate: true
+            }
+        }
+    });
 </script>
--- lb3_backend/server/server.js
+++ lb3_backend/server/server.js
@@ -5,9 +5,9 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
+app.start = () => {
   // start the web server
-  return app.listen(function() {
+  return app.listen(() => {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -20,10 +20,17 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, (err) => {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) {
+    app.io = require('socket.io')(app.start());
+    app.io.on('connect', (socket) => {
+      console.log('a user connected');
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    });
+  }
 });
