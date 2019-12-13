566da65896f28019dc38cff6a93a0e82b036d67a
rogerxaic <roger.miret@gmail.com>
2019-12-13 01:02:27 +0100

    Add notifications

---
+++ demo_app/src/components/Notification.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="notification-container">
+    <div class="notification-actual-content">
+      <h3>{{notification.title}}</h3>
+      <div>{{notification.description}}</div>
+    </div>
+    <div class="actions">
+      <div class="action" v-for="action in notification.actions" v-on:click="() => action.callback()">
+        {{action.title}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Notification",
+        props: ['notification']
+    }
+</script>
+
+<style scoped lang="scss">
+  $radius: 10px;
+  $border-color: #c8c7cc;
+
+  .notification-container {
+    position: relative;
+    background-color: white;
+    border-radius: $radius;
+    min-width: 40vh;
+
+    .notification-actual-content {
+      padding: 30px;
+    }
+
+    .actions {
+      display: flex;
+      border-top: 1px solid $border-color;
+
+      .action {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 10px;
+        cursor: pointer;
+        color: #0076ff;
+        border-right: 1px solid $border-color;
+
+        &:first-of-type {
+          border-bottom-left-radius: $radius;
+        }
+
+        &:last-of-type {
+          font-weight: bold;
+          border-bottom-right-radius: $radius;
+          border-right-color: transparent;
+        }
+
+        &:hover {
+          background-color: #e2e2e2;
+        }
+      }
+    }
+  }
+</style>
---
+++ demo_app/src/components/NotificationCenter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="notification-centre-container" :class="{active: notification}">
+    <notification v-if="notification" :notification="notification"/>
+  </div>
+</template>
+
+<script>
+    import Notification from "@/components/Notification";
+    import {EventBus} from '@/event-bus';
+
+    export default {
+        name: "NotificationCenter",
+        components: {Notification},
+        data() {
+            return {
+                notification: null,
+            }
+        },
+        mounted() {
+            EventBus.$on('notification', notification => this.notification = notification);
+            EventBus.$on('dismiss-notification', () => this.notification = null);
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+  .notification-centre-container {
+    background-color: #4a5c5fa3;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: calc(50vh - 21px);
+    z-index: 3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    visibility: hidden;
+
+    &.active {
+      visibility: visible;
+    }
+  }
+</style>
